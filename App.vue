786ca952b2fb7235b1f4bef119690756e0613630
lessfish <lessfish@outlook.com>
2019-08-01 21:08:31 +0800

    递归组件

---
+++ App.vue
@@ -0,0 +1,79 @@
+<template>
+  <List :data="data"/>
+</template>
+
+<script>
+import List from './List'
+
+export default {
+  data () {
+    return {
+      data: [
+        {
+          title: '体育明星',
+          children: [
+            {
+              title: '游泳',
+              children: [
+                {
+                  title: '孙杨'
+                },
+                {
+                  title: '傅园慧'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          title: '篮球',
+          children: [
+            {
+              title: 'NBA',
+              children: [
+                {
+                  title: '勇士',
+                  children: [
+                    {
+                      title: '库里',
+                      children: [
+                        {
+                          title: 'MVP'
+                        },
+                        {
+                          title: '得分王'
+                        }
+                      ]
+                    },
+                    {
+                      title: '汤普森'
+                    }
+                  ]
+                },
+                {
+                  title: '火箭',
+                  children: [
+                    {
+                      title: '哈登'
+                    },
+                    {
+                      title: '维斯布鲁克'
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  },
+  components: {
+    List
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ List.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="list-container">
+    <div v-for="(item, index) in data" :key="index">
+      <div>{{ item.title }}</div>
+      <List v-if="item.children" :data="item.children"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style>
+.list-container {
+  margin-left: 10px
+}
+</style>
